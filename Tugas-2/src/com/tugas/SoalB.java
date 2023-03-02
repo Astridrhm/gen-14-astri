a5f58049405ce33c1877e0fb5d8a4b55df90fdc0
astridrhm <astridrhm@astridrhm>
2023-03-02 10:49:13 +0700

    Test

--- Tugas-2/src/com/tugas/SoalB.java
+++ Tugas-2/src/com/tugas/SoalB.java
@@ -14,10 +14,7 @@ public class SoalB {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Nilai n : ");
 		int n = input.nextInt();
-<<<<<<< HEAD
 		input.close();
-=======
->>>>>>> branch 'master' of git@github.com:Astridrhm/gen-14-astri.git
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
